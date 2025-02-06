a139d4ff47457fe3a3f4e3903d1dc5814c3bda76
robinPecheurBstorm <robin.pecheur@bstorm.be>
2025-02-06 10:53:14 +0100

    Exercice formulaire

---
+++ DemoBlazor/Models/Utilisateur.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoBlazor.Models
+{
+	public class Utilisateur
+	{
+        [Required]
+        public string Nom { get; set; }
+
+		[Required]
+		[EmailAddress]
+        public string Email { get; set; }
+
+		[Required]
+		[Phone]
+        public string Telephone { get; set; }
+
+
+		[Required]
+		public string Message { get; set; }
+
+		[Required]
+		public string CouleurPreferee { get; set; }
+    }
+}
---
+++ DemoBlazor/Pages/ExerciceFormulaire/FormulaireContact.razor.cs
@@ -0,0 +1,24 @@
+﻿using DemoBlazor.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DemoBlazor.Pages.ExerciceFormulaire
+{
+	public partial class FormulaireContact: ComponentBase
+	{
+		public Utilisateur utilisateur { get; set; } = new Utilisateur();
+
+        public bool FormulaireValideEtEnvoye { get; set; } = false;
+		public bool MessageErreur { get; set; } = false;
+
+        public void EnvoyerFormulaireValide()
+		{
+			MessageErreur = false;
+			FormulaireValideEtEnvoye = true;
+		}
+		public void SignalerFormulaireInvalide()
+		{
+			MessageErreur = true;
+		}
+
+	}
+}
